9dc80703de41f72b28f2d9503fe18f11c54f650e
unknown <srjspnckr1997@gmail.com>
2023-08-09 12:43:16 +0530

    token end

--- middlewares/routermiddleware.js
+++ middlewares/routermiddleware.js
@@ -1,2 +1,15 @@
-
+const jwt = require("jsonwebtoken");
 //middleware
+const jwtMiddleware = (req, res, next) => {
+  try {
+    //access token from request header
+    const token = req.headers["access_token"]; //no token- runtime error
+    //validate token - jwt -verify()
+    jwt.verify(token, "secretkey123"); //- true/false
+    //if token is verified continue the request
+    next();
+  } catch {
+    res.status(404).json("please login");
+  }
+};
+module.exports = jwtMiddleware;
--- routes/userRouting.js
+++ routes/userRouting.js
@@ -1,5 +1,6 @@
 const express=require('express')
 const logic=require('../controllers/logic')
+const jwtMiddleware = require('../middlewares/routermiddleware')
 //Create an object for Router class in express
 const router=new express.Router()
 //application specific middleware - true/false
@@ -9,15 +10,15 @@ router.post('/bankuser/user-register',logic.register)
 //login
 router.post('/bankuser/user-login',logic.login)
 //user profile
-router.get('/bankuser/user-profile/:acno',logic.getProfile)
+router.get('/bankuser/user-profile/:acno',jwtMiddleware,logic.getProfile)
 //balance enquiry
-router.get('/bankuser/bal-enquiry/:acno',logic.getBalance)
+router.get('/bankuser/bal-enquiry/:acno',jwtMiddleware,logic.getBalance)
 //money transfer
-router.post('/bankuser/money-transfer',logic.moneyTransfer)
+router.post('/bankuser/money-transfer',jwtMiddleware,logic.moneyTransfer)
 //transaction history
-router.get('/bankuser/user-history/:acno',logic.history)
+router.get('/bankuser/user-history/:acno',jwtMiddleware,logic.history)
 //delete ac
-router.delete('/bankuser/user-delete/:acno',logic.deleteAc)
+router.delete('/bankuser/user-delete/:acno',jwtMiddleware,logic.deleteAc)
 //export router
 module.exports=router
 
